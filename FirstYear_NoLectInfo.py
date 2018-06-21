f2d0623860ab67bbdd79ce8447bd8af45bb194f2
yukito <wisteria.friend@gmail.com>
2018-06-21 15:09:51 +0900

    改良

--- FirstYear_NoLectInfo.py
+++ FirstYear_NoLectInfo.py
@@ -2,7 +2,7 @@ import time, csv
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-csvFile = open(r"C:\Users\Tomoyuki Fujiwara\files\FirstYear_NoLectInfo.csv",'w+',newline='', encoding='CP932', errors='ignore')
+csvFile = open("files/FirstYear_NoLectInfo.csv",'w+',newline='', encoding='CP932', errors='ignore')
 writer = csv.writer(csvFile)
 
 driver = webdriver.Chrome(executable_path = r'C:\Users\Tomoyuki Fujiwara\Downloads\chromedriver_win32\chromedriver')
@@ -18,7 +18,7 @@ try:
         bsObj = BeautifulSoup(source, 'lxml')
         table = bsObj.findAll("table")[2]
         tr = table.findAll("tr")
-        sptr = tr[1:]
+        sptr = tr[1:-1]
 
         for trs in sptr:
             csvRow = []
@@ -26,17 +26,31 @@ try:
                 tdstr = str(td)
                 if '<br/>' in tdstr:
                     tdspbr = tdstr.split("<br/>")
-                    for cell in tdspbr:
+                    csvex = [""] * 4
+                    for cell, i in zip(tdspbr, range(4)):
                         cellbso = BeautifulSoup(cell, "lxml")
                         celllet = cellbso.get_text()
-                        csvRow.append(celllet)
+                        csvex[i] = celllet
+                    el1 = csvex[1]
+                    el2 = csvex[2]
+                    csvex[1] = el2
+                    csvex[2] = el1
+                    if ',' in csvex[0]:
+                        csvex2 = csvex[0].split(",")
+                        csvex[0] = csvex2[0]
+                        csvex[1] = csvex2[1]
+                    if ',' in csvex[2]:
+                        csvex3 = csvex[2].split(",")
+                        csvex[2] = csvex3[0]
+                        csvex[3] = csvex3[1]
+                    csvRow.extend(csvex)
                 else: 
                     cellbso = BeautifulSoup(tdstr, "lxml")
                     celllet = cellbso.get_text()
                     csvRow.append(celllet)
             writer.writerow(csvRow)
 
-        if 'value="����"' in source:
+        if 'value="次へ"' in source:
             if '<span>1</span>' in source:
                 Search = driver.find_element_by_name('rdlGrid$gridList$ctl14$ctl01')
             else:
