7666acfb96ce487965eadf23acaebef0dbeb1515
yukito <wisteria.friend@gmail.com>
2018-07-01 21:38:22 +0900

    #1 データベース対応、chromeをヘッドレスモードへ変更

--- NoLectInfo/Engineering_NoLectInfo.py
+++ NoLectInfo/Engineering_NoLectInfo.py
@@ -1,30 +1,64 @@
-import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
-#csvファイルを展開（任意のディレクトリにて）及びwriterのセット
-csvFile = open(".\Engineering_NoLectInfo.csv",'w+',newline='')
-writer = csv.writer(csvFile)
-
-#指定したURLのHTMLソースをbeautiful soup固有のオブジェクトとして格納、
-html = urlopen("https://www.eng.hokudai.ac.jp/lecinfo/")
-bsObj = BeautifulSoup(html, "lxml")
-
-#「tbody」にtbodyタグの0番目の内容を格納
-tbody = bsObj.findAll("tbody")[0]
+import mysql.connector
 
 try:
-    #「tbodies」に「tbody」内のtrタグの内容を順に格納
-    for tbodies in tbody.findAll("tr"):
-        csvRow = []
-        #「cell」に「tbodies」内のtdタグの内容を順に格納
-        for cell in tbodies.findAll("td"):
-            #「cell」からテキストデータのみを抽出し「csvRow」に配列として順次格納   
-            celllet = cell.get_text()
-            csvRow.append(celllet)
-        #n番目のtrタグの内容をcsvファイルのn列に書き込み
-        writer.writerow(csvRow) 
+    #指定したURLのHTMLソースをbeautiful soup固有のオブジェクトとして格納、
+    html = urlopen("https://www.eng.hokudai.ac.jp/lecinfo/")
+    bsObj = BeautifulSoup(html, "lxml")
 
-finally:
-    #csvファイルを閉じ終了
-    csvFile.close()
+    # データの選別（最初のtbodyタブを選択)
+    rows = bsObj.findAll("tbody")[0]
+
+    # データの整理
+    data_table = []
+    for row in rows.findAll("tr"):
+        data_record = []
+        for cell in row.findAll("td"): 
+            text = cell.get_text()
+            data_record.append(text)
+        data_table.append(data_record)
+
+    # データベース接続
+    con = mysql.connector.connect(
+        host='127.0.0.1',
+        db='hokudai_app',
+        user='writer',
+        passwd='write'
+    )
+
+    """
+    「hokudai_app」データベースに「engineering_nolectinfo」テーブルがある場合はそのまま
+    なければ
+    CREATE TABLE engineering_nolectinfo (日付 VARCHAR(15), 講時 VARCHAR(2), 区分 VARCHAR(50), 講義科目名 VARCHAR(100), 担当 VARCHAR(50), 講義室 VARCHAR(50), 備考 VARCHAR(200), 更新 VARCHAR(17));
+    で作成
+    """
+
+    # 辞書型カーソル取得
+    cur = con.cursor(dictionary=True)
+    # データの削除
+    cur.execute('delete from engineering_nolectinfo')
+    # データの入力
+    cur.executemany("insert into engineering_nolectinfo (日付, 講時, 区分, 講義科目名, 担当, 講義室, 備考, 更新) values (%s, %s, %s, %s, %s, %s, %s, %s)", data_table)
+    # コミットして変更を確定
+    con.commit()
+
+except:
+    # データベース接続
+    con = mysql.connector.connect(
+        host='127.0.0.1',
+        db='hokudai_app',
+        user='writer',
+        passwd='write'
+    )
+    # カーソル取得
+    cur = con.cursor()
+    # データの削除
+    cur.execute('delete from engineering_nolectinfo')
+    # エラーメッセージ表示
+    print('Error!!')
+
+finally:    
+    # 切断
+    cur.close()
+    con.close()
--- NoLectInfo/FirstYear_NoLectInfo.py
+++ NoLectInfo/FirstYear_NoLectInfo.py
@@ -1,66 +1,127 @@
-import time, csv, os
-from selenium import webdriver
+import time, os
+from selenium.webdriver import Chrome, ChromeOptions
 from bs4 import BeautifulSoup
+import mysql.connector
 
-driver = webdriver.Chrome(executable_path = os.path.abspath('..\webscrapying') + '\chromedriver.exe')
-driver.get('http://inform.academic.hokudai.ac.jp/webinfo/p/SerchInfo.aspx?mode=cancel')
-time.sleep(5)
-search_box = driver.find_element_by_name('btnSerch')
-search_box.click()
-Sources = []
-time.sleep(5)
-
-while True:
-    source = driver.page_source
-    Sources.append(source)
-    if 'value="次へ"' in source:
-        if '<span>1</span>' in source:
-            Search = driver.find_element_by_name('rdlGrid$gridList$ctl14$ctl01')
-        else:
-            Search = driver.find_element_by_name('rdlGrid$gridList$ctl14$ctl03')
-        Search.click()
-        time.sleep(5)
-    else:
-        break
-driver.quit()
-
-csvFile = open(".\FirstYear_NoLectInfo.csv",'w+',newline='', encoding='CP932', errors='ignore')
-writer = csv.writer(csvFile)
+# 「name」を名前に持つボタンをクリックする関数
+def buttonclick(name):
+    button = driver.find_element_by_name(name)
+    button.click()
 
 try:
-    for text in Sources:
-        bsObj = BeautifulSoup(text, 'lxml')
+    # ウェブドライバー起動
+    options = ChromeOptions()
+    # ヘッドレスモードを有効にする（次の行をコメントアウトすると画面が表示される）。
+    options.add_argument('--headless')
+    # ChromeのWebDriverオブジェクトを作成する。
+    driver = Chrome(options = options, executable_path = os.path.abspath(r'.\webscrapying') + r'\chromedriver.exe')
+    driver.get('http://inform.academic.hokudai.ac.jp/webinfo/p/SerchInfo.aspx?mode=cancel')
+    time.sleep(5)
+    # 目標のページに移動
+    buttonclick('btnSerch')
+    time.sleep(5)
+
+    # 最終的にデータベースのテーブルとなる配列
+    data_table = []
+    while True: 
+        # HTMLソースをbeautifulsoup固有のオブジェクトとして格納
+        source = driver.page_source
+        bsObj = BeautifulSoup(source, 'lxml')
+        # データの選別
         table = bsObj.findAll("table")[2]
-        tr = table.findAll("tr")
-        sptr = tr[1:-1]
-        for trs in sptr:
-            csvRow = []
-            for td in trs.findAll("td"):
-                tdstr = str(td)
-                if '<br/>' in tdstr:
-                    tdspbr = tdstr.split("<br/>")
-                    csvex = [""] * 4
-                    for cell, i in zip(tdspbr, range(4)):
+        rows = table.findAll("tr")
+        specific_rows = rows[1:-1]
+
+        # データ整理
+        for row in specific_rows:
+            # 最終的にデータベースのレコードとなる配列
+            data_record = []
+            for cell in row.findAll("td"):
+                cellstr = str(cell)
+                # 細分したデータに<br/>が含まれていた場合の例外処理
+                if '<br/>' in cellstr:
+                    cellspbr = cellstr.split("<br/>")
+                    cellex = [""] * 4
+                    for cell, i in zip(cellspbr, range(4)):
                         cellbso = BeautifulSoup(cell, "lxml")
                         celllet = cellbso.get_text()
-                        csvex[i] = celllet
-                    el1 = csvex[1]
-                    el2 = csvex[2]
-                    csvex[1] = el2
-                    csvex[2] = el1
-                    if ',' in csvex[0]:
-                        csvex2 = csvex[0].split(",")
-                        csvex[0] = csvex2[0]
-                        csvex[1] = csvex2[1]
-                    if ',' in csvex[2]:
-                        csvex3 = csvex[2].split(",")
-                        csvex[2] = csvex3[0]
-                        csvex[3] = csvex3[1]
-                    csvRow.extend(csvex)
+                        cellex[i] = celllet
+                    el1 = cellex[1]
+                    el2 = cellex[2]
+                    cellex[1] = el2
+                    cellex[2] = el1
+                    if ',' in cellex[0]:
+                        cellex2 = cellex[0].split(",")
+                        cellex[0] = cellex2[0]
+                        cellex[1] = cellex2[1]
+                    if ',' in cellex[2]:
+                        cellex3 = cellex[2].split(",")
+                        cellex[2] = cellex3[0]
+                        cellex[3] = cellex3[1]
+                    data_record.extend(cellex)
+                # 問題がなければそのままdata_record配列の一要素として格納
                 else: 
-                    cellbso = BeautifulSoup(tdstr, "lxml")
+                    cellbso = BeautifulSoup(cellstr, "lxml")
                     celllet = cellbso.get_text()
-                    csvRow.append(celllet)
-            writer.writerow(csvRow)
-finally:
-    csvFile.close()
+                    data_record.append(celllet)
+            del data_record[10]
+            del data_record[11]
+            data_table.append(data_record)
+        
+        # ページ遷移の処理
+        if 'value="次へ"' in source:
+            if '<span>1</span>' in source:
+                buttonclick('rdlGrid$gridList$ctl14$ctl01')
+            else:
+                buttonclick('rdlGrid$gridList$ctl14$ctl03')
+            time.sleep(5)
+        else:
+            break
+
+    # ドライバー切断
+    driver.quit()
+    
+    # データベース接続
+    con = mysql.connector.connect(
+        host='127.0.0.1',
+        db='hokudai_app',
+        user='writer',
+        passwd='write'
+    )
+
+    """
+    「hokudai_app」データベースに「firstyear_nolectinfo」テーブルがある場合はそのまま
+    なければ
+    CREATE TABLE firstyear_nolectinfo (休講日 VARCHAR(10), 曜日・時限 VARCHAR(2), 曜日・時限2 VARCHAR(2), Day・Period  VARCHAR(4), Day・Period2  VARCHAR(4), 科目名 VARCHAR(50), 講義題目 VARCHAR(100), CourseTitle VARCHAR(50), CourseSubTitle VARCHAR(100), 教員名 VARCHAR(50), Instructor VARCHAR(50));
+    で作成
+    """
+
+    # 辞書型カーソル取得
+    cur = con.cursor(dictionary=True)
+    # データの削除
+    cur.execute('delete from firstyear_nolectinfo')
+    # データの入力
+    cur.executemany("insert into firstyear_nolectinfo (休講日, 曜日・時限, 曜日・時限2, Day・Period, Day・Period2, 科目名, 講義題目, CourseTitle, CourseSubTitle, 教員名, Instructor) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", data_table)
+    # コミットして変更を確定
+    con.commit()
+
+except:
+    # データベース接続
+    con = mysql.connector.connect(
+        host='127.0.0.1',
+        db='hokudai_app',
+        user='writer',
+        passwd='write'
+    )
+    # カーソル取得
+    cur = con.cursor()
+    # データの削除
+    cur.execute('delete from firstyear_nolectinfo')
+    # エラーメッセージ表示
+    print('Error!!')
+
+finally:    
+    # データベース切断
+    cur.close()
+    con.close()
+    print('Done')
